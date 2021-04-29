3eadcba45f30e545e46139a7af44c33c22d288c2
Nickolai Paromov <paromovn@goldmail.etsu.edu>
2021-04-28 20:28:33 -0500

    reorganized folder structure

---
+++ BowlingScoreCalculatorTests.Unit/FrameTests.cs
@@ -0,0 +1,22 @@
+﻿using BowlingScoreCalculator.Domain;
+using FluentAssertions;
+using Xunit;
+
+namespace BowlingScoreCalculatorTests.Unit
+{
+    public class FrameTests
+    {
+        [Theory]
+        [InlineData(3, 4, 7)]
+        [InlineData(7, 3, 15)]
+        [InlineData(10, 0, 19)]
+        public void FrameCalculatesScore(int roll1, int roll2, int expected)
+        {
+            var sut = new Frame(1, new []{ roll1, roll2 });
+
+            var result = sut.CalculateScore(5, 4);
+
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ BowlingScoreCalculatorTests.Unit/ScoreboardTests.cs
@@ -0,0 +1,64 @@
+﻿using BowlingScoreCalculator.Domain;
+using FluentAssertions;
+using Xunit;
+
+namespace BowlingScoreCalculatorTests.Unit
+{
+    public class ScoreboardTests
+    {
+        [Theory]
+        [InlineData(5, 3, 2, 0, 10)]
+        [InlineData(3, 7, 6, 4, 26)]
+        [InlineData(3, 7, 2, 0, 14)]
+        [InlineData(10, 0, 2, 0, 14)]
+        [InlineData(10, 0, 2, 4, 22)]
+        public void ScoreboardCalculatesScoresCorrectly(int roll1, int roll2, int roll3, int roll4, int expected)
+        {
+            int[][] frames = new[]
+            {
+                new[]{ roll1, roll2},
+                new[]{ roll3, roll4},
+                new[]{0, 0},
+                new[]{0, 0},
+                new[]{0, 0},
+                new[]{0, 0},
+                new[]{0, 0},
+                new[]{0, 0},
+                new[]{0, 0},
+                new[]{0, 0},
+            };
+            var sut = new Scoreboard(frames);
+
+            var result = sut.CalculateFinalScore();
+
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(5, 3, 0, 8)]
+        [InlineData(5, 5, 2, 12)]
+        [InlineData(10, 5, 7, 22)]
+        [InlineData(10, 10, 10, 30)]
+        public void ScoreboardCalculatesFrame10ScoresCorrectly(int roll1, int roll2, int roll3, int expected)
+        {
+            int[][] frames = new[]
+            {
+                new[]{ 0, 0},
+                new[]{ 0, 0},
+                new[]{0, 0},
+                new[]{0, 0},
+                new[]{0, 0},
+                new[]{0, 0},
+                new[]{0, 0},
+                new[]{0, 0},
+                new[]{0, 0},
+                new[]{ roll1, roll2, roll3},
+            };
+            var sut = new Scoreboard(frames);
+
+            var result = sut.CalculateFinalScore();
+
+            result.Should().Be(expected);
+        }
+    }
+}
